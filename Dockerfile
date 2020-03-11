9ac2d1b77aede8db1a504df312378be066bba4dc
Ariejan de Vroom <ariejan@devroom.io>
2020-03-11 12:46:00 +0100

    Download / install specified hugo version

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,13 @@ FROM alpine:3.11
 
 LABEL maintainer="Ariejan de Vroom <ariejan@devroom.io>"
 
-RUN apk add --no-cache ca-certificates mailcap git
+RUN apk add --no-cache \
+    ca-certificates \
+    mailcap \
+    git \
+    wget \
+    libc6-compat \
+    libstdc++
 
 ADD drone-hugo.sh /bin/
 RUN chmod +x /bin/drone-hugo.sh
--- drone-hugo.sh
+++ drone-hugo.sh
@@ -3,9 +3,12 @@
 PLUGIN_HUGO_VERSION="0.67.0"
 HUGO_ARCH="64bit"
 
-echo "Fetching Hugo ${PLUGIN_HUGO_VERSION}"
-wget -O- https://github.com/gohugoio/hugo/releases/download/v${PLUGIN_HUGO_VERSION}/hugo_${PLUGIN_HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz | tar xz -C /bin
+HUGO_URL="https://github.com/gohugoio/hugo/releases/download/v${PLUGIN_HUGO_VERSION}/hugo_extended_${PLUGIN_HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz"
 
+echo "Fetching Hugo ${PLUGIN_HUGO_VERSION} from ${HUGO_URL}"
+wget -O- ${HUGO_URL} | tar xz -C /usr/local/bin
+
+git --version
 hugo version
 
-
+hugo
